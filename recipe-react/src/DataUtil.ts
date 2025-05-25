ba18911b4cc89337a846925c415b9de791970e51
yochevedg <yochevedgoldberg1@gmail.com>
2025-05-25 17:45:11 -0400

    react utils and more

--- recipe-react/src/DataUtil.ts
+++ recipe-react/src/DataUtil.ts
@@ -1,58 +1,36 @@
 import { FieldValues } from "react-hook-form";
 import { ICuisine, IRecipe, IUsers } from "./DataInterfaces";
+import { createAPI, getUserStore } from "@yochevedg/reactutils";
 
 const baseurl = "https://recipeapi-yg.azurewebsites.net/api/"
-//"https://localhost:44370/api/"
 
-async function fetchData<T>(url: string): Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url);
-    const data = await r.json();
-    return data;
-}
-
-async function deleteData<T>(url: string): Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url, { method: "DELETE" });
-    const data = await r.json();
-    return data;
-}
-
-async function postData<T>(url: string, form: FieldValues): Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(form),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-    const data = await r.json();
-    return data;
+function api() {
+    const sessionkey = getUserStore(baseurl).getState().sessionKey;
+    return createAPI(baseurl, sessionkey);
 }
 
 export async function fetchRecipes() {
-    return await fetchData<IRecipe[]>("recipe");
+    return await api().fetchData<IRecipe[]>("recipe");
 }
 
 export async function fetchCuisine() {
-    return await fetchData<ICuisine[]>("cuisine");
+    return await api().fetchData<ICuisine[]>("cuisine");
 }
 
 export async function fetchRecipesByCuisineId(cuisineId: number) {
-    return await fetchData<IRecipe[]>(`Recipe/getbycuisine/${cuisineId}`);
+    return await api().fetchData<IRecipe[]>(`Recipe/getbycuisine/${cuisineId}`);
 }
 
 export async function fetchUsers() {
-    return await fetchData<IUsers[]>("recipe/users");
+    return await api().fetchData<IUsers[]>("recipe/users");
 }
 
 export async function postRecipe(form: FieldValues) {
-    return postData<IRecipe>("recipe", form);
+    return api().postData<IRecipe>("recipe", form);
 }
 
 export async function deleteRecipe(recipeId: number) {
-    return deleteData<IRecipe>(`recipe?id=${recipeId}`);
+    return api().deleteData<IRecipe>(`recipe?id=${recipeId}`);
 }
 
 export const blankrecipe: IRecipe = {
